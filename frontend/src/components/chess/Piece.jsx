cba2c18ee5bf7ad473dbec9dad47bff42cd749f7
79354 <gillramansingh900@gmail.com>
2025-12-03 16:57:43 +0530

    added pieces

--- frontend/src/components/chess/Piece.jsx
+++ frontend/src/components/chess/Piece.jsx
@@ -1,5 +1,51 @@
-function Piece() {
+function Piece({ type, color, isDraggable }) {
+  // Using chess.com style SVG pieces from CDN
+  const pieceImages = {
+    white: {
+      king: "https://images.chesscomfiles.com/chess-themes/pieces/neo/150/wk.png",
+      queen:
+        "https://images.chesscomfiles.com/chess-themes/pieces/neo/150/wq.png",
+      rook: "https://images.chesscomfiles.com/chess-themes/pieces/neo/150/wr.png",
+      bishop:
+        "https://images.chesscomfiles.com/chess-themes/pieces/neo/150/wb.png",
+      knight:
+        "https://images.chesscomfiles.com/chess-themes/pieces/neo/150/wn.png",
+      pawn: "https://images.chesscomfiles.com/chess-themes/pieces/neo/150/wp.png",
+    },
+    black: {
+      king: "https://images.chesscomfiles.com/chess-themes/pieces/neo/150/bk.png",
+      queen:
+        "https://images.chesscomfiles.com/chess-themes/pieces/neo/150/bq.png",
+      rook: "https://images.chesscomfiles.com/chess-themes/pieces/neo/150/br.png",
+      bishop:
+        "https://images.chesscomfiles.com/chess-themes/pieces/neo/150/bb.png",
+      knight:
+        "https://images.chesscomfiles.com/chess-themes/pieces/neo/150/bn.png",
+      pawn: "https://images.chesscomfiles.com/chess-themes/pieces/neo/150/bp.png",
+    },
+  };
 
+  const pieceUrl = placeImages[color]?.[type];
+
+  if (!pieceUrl) return null;
+
+  return (
+    <img
+      src={pieceUrl}
+      alt={`${color} ${type}`}
+      className={`
+        w-full h-full p-1 select-none pointer-events-none
+        ${
+          isDraggable
+            ? "cursor-grab active:cursor-grabbing"
+            : "cursor-not-allowed"
+        }
+        transition-transform duration-100
+        hover:scale-110
+      `}
+      draggable={false}
+    />
+  );
 }
 
 export default Piece;
