f429418bb092afc4008fc1c8c5cb16e7d7f2272f
79354 <gillramansingh900@gmail.com>
2025-12-08 20:50:25 +0530

    asgi setup

--- server/core/asgi.py
+++ server/core/asgi.py
@@ -1,9 +1,20 @@
 import os
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
+from channels.security.websocket import AllowedHostsOriginValidator
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 
+django_asgi_app = get_asgi_application()
+
+from game.routing import websocket_urlpatterns
+
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
+    "websocket": AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter(websocket_urlpatterns)
+        )
+    ),
 })
