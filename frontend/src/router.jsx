7971a25665d5d40b1ae1435ff92656f616e6bc7b
79354 <gillramansingh900@gmail.com>
2025-12-08 21:28:02 +0530

    updated app router imports

--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -1,32 +1,41 @@
 import React from "react";
-import { Routes, Route } from 'react-router-dom';
-import Navbar from './components/layout/NavBar'
-import Sidebar from './components/layout/Sidebar'
-import Footer from './components/layout/Footer'
-import ProtectedRoute from './components/ProtectedRoute'
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Navbar from './components/layout/NavBar';
+import Sidebar from './components/layout/Sidebar';
+import Footer from './components/layout/Footer';
+import ProtectedRoute from './components/ProtectedRoute';
 
 import Register from "./pages/Register";
-import Login from "./pages/Login"
+import Login from "./pages/Login";
 import NotFound from './pages/NotFound';
-import Home from './pages/Home'
-import Profile from './pages/Profile'
+import Home from './pages/Home';
+import Game from './pages/Game';
+import Profile from './pages/Profile';
+import Matchmaking from './pages/Matchmaking';
+import Lobby from './pages/Lobby';
+import Friends from './pages/Friends';
+import Spectate from './pages/Spectate';
+
 import { AuthProvider } from "./context/AuthContext";
+import { GameProvider } from "./context/GameContext";
 
 function AppRouter() {
     return (
         <AuthProvider>
-            <Routes>
-                <Route path="/login" element />
-                <Route path="/register" element />
+            <GameProvider>
+                <Routes>
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
 
-                <Route path="/*" 
-                    element={
-                    <ProtectedRoute>
-                        <MainLayout />
-                    </ProtectedRoute>
-                    } 
-                />
-            </Routes>
+                    <Route path="/*" 
+                        element={
+                        <ProtectedRoute>
+                            <MainLayout />
+                        </ProtectedRoute>
+                        } 
+                    />
+                </Routes>
+            </GameProvider>
         </AuthProvider>
     );
 }
@@ -41,6 +50,10 @@ function MainLayout() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/game/:gameId" element={<Game />} />
+            <Route path="/matchmaking" element={<Matchmaking />} />
+            <Route path="/lobby" element={<Lobby />} />
+            <Route path="/friends" element={<Friends />} />
+            <Route path="/spectate" element={<Spectate />} />
             <Route path="/profile/:username" element={<Profile />} />
             <Route path="/404" element={<NotFound />} />
             <Route path="*" element={<Navigate to="/404" replace />} />
